95d017d3678f2cd7b3902954234e19de2bb20072
Bikram Ghuku <bikramghuku05@gmail.com>
2024-02-23 21:54:24 +0530

    chore: added search UI for chat selector

---
+++ src/components/SearchArea.jsx
@@ -0,0 +1,17 @@
+'use client'
+
+import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Search } from 'lucide-react'
+
+function SearchArea() {
+	return (
+		<div className="flex flex-row w-full max-w-sm items-center space-x-10 pr-10">
+			<Input type="email" placeholder="Email" />
+			<Button variant={'default'}><Search /></Button>
+		</div>
+	)
+}
+
+export default SearchArea
