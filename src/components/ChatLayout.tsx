4c09aedbf594d67fd58331a69b2e970a5e2ae7d8
Bikram Ghuku <bikramghuku05@gmail.com>
2024-02-23 21:55:10 +0530

    fix: added logo to header, chat selector, added search area to chat selector

--- src/components/ChatLayout.tsx
+++ src/components/ChatLayout.tsx
@@ -1,19 +1,28 @@
+'use client'
+
 import React from 'react'
+import SearchArea from '@/components/SearchArea'
+import Image from 'next/image'
 
 function ChatLayout() {
-  return (
-    <div className='flex flex-1'>
-        <div className="flex flex-col w-1/4">
-            <div className='flex h-1/6 w-full'>
-                { /* Seatch and app name*/}
-            </div>
-            <div className='flex h-5/6 w-full'>
-                { /* Contacts and names*/}
-            </div>
-        </div>
-        <div className="flex w-3/4"></div>
-    </div>
-  )
+	return (
+		<div className="flex flex-1">
+			<div className="flex flex-col w-1/4 dark:bg-gray-900 bg-[#ffffff]">
+				<div className="flex h-1/6 w-full flex-col pt-5 pl-5">
+					{/* Seatch and app name*/}
+					<div className="flex flex-row pb-5 pl-5 pr-10">
+                        <Image src="/icon.png" alt="ChatSync" width="50" height="50" objectFit='cover' />
+						<div className="font-extrabold lg:text-5xl text-center lg:pl-10 text-[#00A3FF] text-xl pl-5">
+							Sync Chat
+						</div>
+					</div>
+					<SearchArea />
+				</div>
+				<div className="flex h-5/6 w-full">{/* Contacts and names*/}</div>
+			</div>
+			<div className="flex w-3/4"></div>
+		</div>
+	)
 }
 
 export default ChatLayout
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,15 @@ import DarkToggler from './DarkToggler'
 import UserButton from './UserButton'
 import ChatsButton from './ChatsButton'
 import GetChats from './GetChats'
+import Image from 'next/image'
 
 function Header() {
 	return (
 		<header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
 			<nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
+				<div className="flex-1 flex items-center justify-start space-x-4">
+					<Image src="/icon.png" alt="ChatSync" width="40" height="40" />
+				</div>
 				<div className="flex-1 flex items-center justify-end space-x-4">
 					<ChatsButton />
 					<GetChats />
