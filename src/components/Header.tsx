874a0b75a7e6622bf3dfb3e822a8c7928344d1b1
Bikram Ghuku <bikramghuku05@gmail.com>
2024-02-23 15:49:17 +0530

    fix: added getChats and addchats option to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,17 @@
 'use client'
 import React from 'react'
-import DarkToggler from './darkToggler'
+import DarkToggler from './DarkToggler'
 import UserButton from './UserButton'
+import ChatsButton from './ChatsButton'
+import GetChats from './GetChats'
 
 function Header() {
 	return (
 		<header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
 			<nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
 				<div className="flex-1 flex items-center justify-end space-x-4">
+					<ChatsButton />
+					<GetChats />
 					<DarkToggler />
 					<UserButton />
 				</div>
