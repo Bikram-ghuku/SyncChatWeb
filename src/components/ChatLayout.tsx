cf27ac5274d8301080382c9c311b5b03922e3cc5
Bikram Ghuku <bikramghuku05@gmail.com>
2024-02-24 14:01:03 +0530

    fix: removed logo and name of app

--- src/components/ChatLayout.tsx
+++ src/components/ChatLayout.tsx
@@ -7,20 +7,17 @@ import Image from 'next/image'
 function ChatLayout() {
 	return (
 		<div className="flex flex-1">
-			<div className="flex flex-col w-1/4 dark:bg-gray-900 bg-[#ffffff]">
-				<div className="flex h-1/6 w-full flex-col pt-5 pl-5">
+			<div className="flex flex-col w-1/6 dark:bg-gray-900 bg-[#ffffff]">
+				<div className="flex h-1/6 w-full flex-col pt-5 pl-5 pr-5">
 					{/* Seatch and app name*/}
-					<div className="flex flex-row pb-5 pl-5 pr-10">
-                        <Image src="/icon.png" alt="ChatSync" width="50" height="50" objectFit='cover' />
-						<div className="font-extrabold lg:text-5xl text-center lg:pl-10 text-[#00A3FF] text-xl pl-5">
-							Sync Chat
-						</div>
-					</div>
 					<SearchArea />
 				</div>
-				<div className="flex h-5/6 w-full">{/* Contacts and names*/}</div>
+				<div className="flex h-4/5 w-full">{/* Contacts and names*/}</div>
 			</div>
-			<div className="flex w-3/4"></div>
+			<div className="flex w-5/6">
+				<div className="flex"></div>
+				<div className='flex bg-[#121212]'></div>
+			</div>
 		</div>
 	)
 }
