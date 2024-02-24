f22795ddad448051df36020e08af4c8a396d1e78
Bikram Ghuku <bikramghuku05@gmail.com>
2024-02-24 18:08:30 +0530

    fix: added border for chats and input area

--- src/components/ChatLayout.tsx
+++ src/components/ChatLayout.tsx
@@ -7,7 +7,7 @@ import InputArea from '@/components/InputArea'
 function ChatLayout() {
 	return (
 		<div className="flex flex-1">
-			<div className="flex flex-col w-1/6 dark:bg-gray-900 bg-[#ffffff]">
+			<div className="flex flex-col w-1/6 dark:bg-gray-900 bg-[#ffffff] border-r-2 border-[#5E5E5E33] dark:border-[#303030]">
 				<div className="flex h-1/6 w-full flex-col pt-5 pl-5 pr-2">
 					{/* Seatch and app name*/}
 					<SearchArea />
@@ -16,7 +16,7 @@ function ChatLayout() {
 			</div>
 			<div className="flex w-5/6 flex-col">
 				<div className="flex h-full w-full"></div>
-				<div className="flex dark:bg-gray-900 w-full bg-[#ffffff] h-16">
+				<div className="flex dark:bg-gray-900 w-full bg-[#ffffff] h-16 border-t-2 border-[#5E5E5E33] dark:border-[#303030]">
 					<InputArea />
 				</div>
 			</div>
