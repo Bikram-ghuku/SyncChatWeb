4c729340752a8a6d290790fce0b7e9f65e2668af
Bikram Ghuku <bikramghuku05@gmail.com>
2024-02-24 17:42:04 +0530

    refactor: removed unwanted imports

--- src/components/SearchArea.jsx
+++ src/components/SearchArea.jsx
@@ -1,9 +1,7 @@
 'use client'
 
 import React from 'react'
-import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { Search } from 'lucide-react'
 
 function SearchArea() {
 	return (
