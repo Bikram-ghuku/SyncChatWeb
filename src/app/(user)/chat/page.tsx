2cb66affe77cf967c5a917f9b136cc97c79dd94b
Bikram Ghuku <bikramghuku05@gmail.com>
2024-02-24 18:14:53 +0530

    fix: added chats landing page

---
+++ src/app/(user)/chat/page.tsx
@@ -0,0 +1,12 @@
+import Header from '@/components/Header'
+import React from 'react'
+
+function ChatHomePage() {
+  return (
+    <div>
+        <Header />
+    </div>
+  )
+}
+
+export default ChatHomePage
