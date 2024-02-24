2d3f806cd45a92154526d079ad36a68d28367f62
Bikram Ghuku <bikramghuku05@gmail.com>
2024-02-24 18:07:58 +0530

    fix: added border for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ import Image from 'next/image'
 
 function Header() {
 	return (
-		<header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
+		<header className="sticky top-0 z-50 bg-white dark:bg-gray-900 border-b-2 border-[#5e5e5e33] dark:border-[#303030]">
 			<nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
 				<div className="flex-1 flex items-center justify-start space-x-4">
 					<Image src="/icon.png" alt="ChatSync" width="40" height="40" />
