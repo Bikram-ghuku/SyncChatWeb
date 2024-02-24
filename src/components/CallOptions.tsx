2b2b3c9006e17e4e4a2c59e85f41a78b60188e3c
Bikram Ghuku <bikramghuku05@gmail.com>
2024-02-24 21:37:31 +0530

    fix: rename CallOptions.jsx to CallOptions.tsx

---
+++ src/components/CallOptions.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Phone, Video, MoreVertical } from 'lucide-react'
+
+function CallOptions() {
+	return (
+		<div className="flex gap-2">
+			<Button variant="ghost" size="icon">
+				<Phone size="1.25rem" />
+			</Button>
+			<Button variant="ghost" size="icon">
+				<Video size="1.25rem" />
+			</Button>
+			<Button>
+				<MoreVertical size="1.25rem" />
+			</Button>
+		</div>
+	)
+}
+
+export default CallOptions
