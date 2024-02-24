c12c9e0aeefd8d4e2ff90ee3c12fe90ed3efe1eb
Bikram Ghuku <bikramghuku05@gmail.com>
2024-02-24 17:41:28 +0530

    fix: prettier fixes, changed width of chats, added InputArea

--- src/components/ChatLayout.tsx
+++ src/components/ChatLayout.tsx
@@ -2,21 +2,23 @@
 
 import React from 'react'
 import SearchArea from '@/components/SearchArea'
-import Image from 'next/image'
+import InputArea from '@/components/InputArea'
 
 function ChatLayout() {
 	return (
 		<div className="flex flex-1">
 			<div className="flex flex-col w-1/6 dark:bg-gray-900 bg-[#ffffff]">
-				<div className="flex h-1/6 w-full flex-col pt-5 pl-5 pr-5">
+				<div className="flex h-1/6 w-full flex-col pt-5 pl-5 pr-2">
 					{/* Seatch and app name*/}
 					<SearchArea />
 				</div>
 				<div className="flex h-4/5 w-full">{/* Contacts and names*/}</div>
 			</div>
-			<div className="flex w-5/6">
-				<div className="flex"></div>
-				<div className='flex bg-[#121212]'></div>
+			<div className="flex w-5/6 flex-col">
+				<div className="flex h-full w-full"></div>
+				<div className="flex dark:bg-gray-900 w-full bg-[#ffffff] h-16">
+					<InputArea />
+				</div>
 			</div>
 		</div>
 	)
