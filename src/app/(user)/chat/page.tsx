33b12981c7c4f931267cc1841532df1abd9ce831
Bikram Ghuku <bikramghuku05@gmail.com>
2024-02-24 20:01:20 +0530

    fix: prettier fix

--- src/app/(user)/chat/page.tsx
+++ src/app/(user)/chat/page.tsx
@@ -2,11 +2,11 @@ import Header from '@/components/Header'
 import React from 'react'
 
 function ChatHomePage() {
-  return (
-    <div>
-        <Header />
-    </div>
-  )
+	return (
+		<div>
+			<Header />
+		</div>
+	)
 }
 
 export default ChatHomePage
