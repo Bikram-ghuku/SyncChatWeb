3c225cb4627f9753c4ad39d55ff8ce5f1fa4a608
Bikram Ghuku <bikramghuku05@gmail.com>
2024-02-24 14:00:22 +0530

    fix: remove search button

--- src/components/SearchArea.jsx
+++ src/components/SearchArea.jsx
@@ -9,7 +9,6 @@ function SearchArea() {
 	return (
 		<div className="flex flex-row w-full max-w-sm items-center space-x-10 pr-10">
 			<Input type="email" placeholder="Email" />
-			<Button variant={'default'}><Search /></Button>
 		</div>
 	)
 }
