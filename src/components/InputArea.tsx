beb8ab6a1e1c3829cd145017a13a3cc32e613bb8
Bikram Ghuku <bikramghuku05@gmail.com>
2024-02-24 19:27:35 +0530

    fix: prettier fixes

--- src/components/InputArea.tsx
+++ src/components/InputArea.tsx
@@ -6,19 +6,22 @@ import { Smile, Mic, Paperclip, SendHorizonal } from 'lucide-react'
 function InputArea() {
 	return (
 		<div className="flex h-full items-center md:pl-10 w-full">
-            <Button variant="ghost" size="icon" className='mr-2'>
-                <Smile />
-            </Button>
-			<Input type="text" placeholder='Type Message...' className='' />
-            <Button variant="ghost" size="icon" className='md:ml-5 md:mr-2 ml-1'>
-                <Mic />
-            </Button>
-            <Button variant="ghost" size="icon" className='md:mr-2 md:ml-2 mr-1'>
-                <Paperclip />
-            </Button>
-            <Button size="icon" className='mr-1 md:mr-5 ml-1 md:ml-5 md:w-[10%] bg-[#00a3ff] gap-2 p-5 w-[15%]'>
-                Send <SendHorizonal size='1rem' />
-            </Button>
+			<Button variant="ghost" size="icon" className="mr-2">
+				<Smile />
+			</Button>
+			<Input type="text" placeholder="Type Message..." className="" />
+			<Button variant="ghost" size="icon" className="md:ml-5 md:mr-2 ml-1">
+				<Mic />
+			</Button>
+			<Button variant="ghost" size="icon" className="md:mr-2 md:ml-2 mr-1">
+				<Paperclip />
+			</Button>
+			<Button
+				size="icon"
+				className="mr-1 md:mr-5 ml-1 md:ml-5 md:w-[10%] bg-[#00a3ff] gap-2 p-5 w-[15%]"
+			>
+				Send <SendHorizonal size="1rem" />
+			</Button>
 		</div>
 	)
 }
