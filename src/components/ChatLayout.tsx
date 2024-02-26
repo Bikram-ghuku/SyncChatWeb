bab29fe949803b93454d16b753803dbd41b4da1d
Bikram Ghuku <bikramghuku05@gmail.com>
2024-02-26 22:51:14 +0530

    fix: prettier fixes

--- src/components/ChatLayout.tsx
+++ src/components/ChatLayout.tsx
@@ -11,11 +11,11 @@ function ChatLayout() {
 	return (
 		<div className="flex flex-1">
 			<div className="flex flex-col w-1/4 dark:bg-gray-900 bg-[#ffffff] border-r-2 border-[#5E5E5E33] dark:border-[#303030]">
-				<div className="flex h-1/6 w-full flex-col pt-5 pl-5 pr-2">
+				<div className="flex h-[4%] w-full flex-col pt-5 pl-5 pr-2">
 					{/* Seatch and app name*/}
 					<SearchArea />
 				</div>
-				<div className="flex h-4/5 w-full">
+				<div className="flex h-[96%] w-full h-max-[96%] mt-10">
 					<AvailChats />
 				</div>
 			</div>
