44722088153009bff49ace07e4a0659166bf300c
Bikram Ghuku <bikramghuku05@gmail.com>
2024-02-26 23:18:45 +0530

    feat: added available chats option

---
+++ src/components/AvailChatElement.tsx
@@ -0,0 +1,23 @@
+'use client'
+import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+
+type user = { name: string; url: string }
+
+function AvailChatElement(userName: user) {
+	return (
+		<div className="h-[6rem] flex mt-5">
+			<Avatar className="h-[5rem] w-[5rem] mt-5">
+				<AvatarFallback>C</AvatarFallback>
+				<AvatarImage src={userName.url} />
+			</Avatar>
+			<div className="mt-8 ml-4 w-1/2">
+				<div className="font-black">{userName.name}</div>
+				<div className="pt-2 font-extralight">Hey how's it going?</div>
+			</div>
+			<div className="mt-8 flex justify-end w-1/4 mr-4">10:46</div>
+		</div>
+	)
+}
+
+export default AvailChatElement
