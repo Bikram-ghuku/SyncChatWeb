324970ee1e4650a97dcd82b7e8b04804714d96df
Bikram Ghuku <bikramghuku05@gmail.com>
2024-02-26 22:41:52 +0530

    feat: added link to chatbutton

--- src/components/ChatsButton.tsx
+++ src/components/ChatsButton.tsx
@@ -2,12 +2,15 @@
 import React from 'react'
 import { Button } from './ui/button'
 import { MessagesSquare } from 'lucide-react'
+import Link from 'next/link'
 
 function ChatsButton() {
 	return (
 		<div>
 			<Button variant={'ghost'}>
-				<MessagesSquare />
+				<Link href="../chat">
+					<MessagesSquare />
+				</Link>
 			</Button>
 		</div>
 	)
