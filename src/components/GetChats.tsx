c41f040309d53d3846b1867d1534a8e000f049bd
Bikram Ghuku <bikramghuku05@gmail.com>
2024-03-01 18:00:52 +0530

    chore: added add chat dialog box

--- src/components/GetChats.tsx
+++ src/components/GetChats.tsx
@@ -2,13 +2,34 @@
 import React from 'react'
 import { MessageSquarePlus } from 'lucide-react'
 import { Button } from './ui/button'
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from '@/components/ui/dialog'
 
 function GetChats() {
 	return (
 		<div>
-			<Button variant={'ghost'}>
-				<MessageSquarePlus />
-			</Button>
+			<Dialog>
+				<DialogTrigger>
+					<Button variant={'ghost'}>
+						<MessageSquarePlus />
+					</Button>
+				</DialogTrigger>
+				<DialogContent>
+					<DialogHeader>
+						<DialogTitle>Enter the chatId</DialogTitle>
+						<DialogDescription>
+							This action cannot be undone. This will permanently delete your
+							account and remove your data from our servers.
+						</DialogDescription>
+					</DialogHeader>
+				</DialogContent>
+			</Dialog>
 		</div>
 	)
 }
