bc7b9daba54477854d2d4090816e217665bca547
Bikram Ghuku <bikramghuku05@gmail.com>
2024-03-03 13:39:29 +0530

    ui: reduced spsacing between timestamp and username

--- src/components/MessageElement.tsx
+++ src/components/MessageElement.tsx
@@ -7,8 +7,8 @@ function MessageElement({ messageInfo }: { messageInfo: message }) {
 		// self messages
 		return (
 			<div className="flex justify-end">
-                <div className='flex flex-col  mr-2'>
-                    <div className='flex flex-row-reverse gap-7 text-[0.75rem] mb-1'>
+                <div className='flex flex-col mr-2'>
+                    <div className='flex flex-row-reverse gap-3 text-[0.75rem] mb-1'>
                         <div>User</div>
                         <div>{messageInfo.timeStamp}</div>
                     </div>
@@ -27,7 +27,7 @@ function MessageElement({ messageInfo }: { messageInfo: message }) {
 				    <UserAvatar url={messageInfo.url} />
                 </div>
                 <div className='flex flex-col ml-5'>
-                    <div className='flex flex-row gap-7 mb-1'>
+                    <div className='flex flex-row gap-3 mb-1'>
                         <div className='text-[0.75rem]'>
                             {messageInfo.user}
                         </div>
