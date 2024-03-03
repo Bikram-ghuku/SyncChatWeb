ac377e307a538ac9c4590f16f6ca1939c025d368
Bikram Ghuku <bikramghuku05@gmail.com>
2024-03-03 10:34:08 +0530

    refactor: refactor the chat ui by adding padding

--- src/components/ChatLayout.tsx
+++ src/components/ChatLayout.tsx
@@ -36,7 +36,7 @@ function ChatLayout({ chatId }: { chatId: number }) {
 						<CallOptions />
 					</div>
 				</div>
-				<div className="flex h-full w-full">
+				<div className="flex h-full w-full p-5">
 					<Messages chatId={String(chatId)} />
 				</div>
 				<div className="flex dark:bg-gray-900 w-full bg-[#ffffff] h-16 border-t-2 border-[#5E5E5E33] dark:border-[#303030]">
