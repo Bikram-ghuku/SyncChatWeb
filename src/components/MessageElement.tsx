0755f0396d881a869c28d26c31be746956219a27
Bikram Ghuku <bikramghuku05@gmail.com>
2024-03-11 20:05:59 +0530

    chore: added chat message viewer

--- src/components/MessageElement.tsx
+++ src/components/MessageElement.tsx
@@ -1,21 +1,28 @@
 'use client'
 import React from 'react'
 import UserAvatar from '@/components/UserAvatar'
-type message = { id: string; message: string; self: boolean; timeStamp: string; user: string, url: string }
+type message = {
+	id: string
+	message: string
+	self: boolean
+	timeStamp: string
+	user: string
+	url: string
+}
 function MessageElement({ messageInfo }: { messageInfo: message }) {
 	if (messageInfo.self) {
 		// self messages
 		return (
 			<div className="flex justify-end">
-                <div className='flex flex-col mr-2'>
-                    <div className='flex flex-row-reverse gap-3 text-[0.75rem] mb-1'>
-                        <div>User</div>
-                        <div>{messageInfo.timeStamp}</div>
-                    </div>
-                    <div className="bg-[#00a3ff] rounded-bl-lg rounded-tl-lg rounded-br-lg w-fit text-end p-2 text-white">
-                        {messageInfo.message}
-                    </div>
-                </div>
+				<div className="flex flex-col mr-2">
+					<div className="flex flex-row-reverse gap-3 text-[0.75rem] mb-1">
+						<div>User</div>
+						<div>{messageInfo.timeStamp}</div>
+					</div>
+					<div className="bg-[#00a3ff] rounded-bl-lg rounded-tl-lg rounded-br-lg w-fit text-end p-2 text-white">
+						{messageInfo.message}
+					</div>
+				</div>
 				<UserAvatar url="" />
 			</div>
 		)
@@ -23,22 +30,18 @@ function MessageElement({ messageInfo }: { messageInfo: message }) {
 		//received messages
 		return (
 			<div className="flex w-fit">
-                <div>
-				    <UserAvatar url={messageInfo.url} />
-                </div>
-                <div className='flex flex-col ml-5'>
-                    <div className='flex flex-row gap-3 mb-1'>
-                        <div className='text-[0.75rem]'>
-                            {messageInfo.user}
-                        </div>
-                        <div className='text-[0.75rem]'>
-                            {messageInfo.timeStamp}
-                        </div>
-                    </div>
-                    <div className="dark:bg-[#292929] rounded-bl-lg rounded-tr-lg rounded-br-lg w-fit text-end flex-1 p-2 bg-white">
-                        {messageInfo.message}
-                    </div>
-                </div>
+				<div>
+					<UserAvatar url={messageInfo.url} />
+				</div>
+				<div className="flex flex-col ml-5">
+					<div className="flex flex-row gap-3 mb-1">
+						<div className="text-[0.75rem]">{messageInfo.user}</div>
+						<div className="text-[0.75rem]">{messageInfo.timeStamp}</div>
+					</div>
+					<div className="dark:bg-[#292929] rounded-bl-lg rounded-tr-lg rounded-br-lg w-fit text-end flex-1 p-2 bg-white">
+						{messageInfo.message}
+					</div>
+				</div>
 			</div>
 		)
 	}
--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,33 +1,38 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import MessageElement from '@/components/MessageElement'
 
-type userData = {name: string, url: string}
+type userData = { name: string; url: string }
 function Messages({ chatId }: { chatId: userData }) {
+	const messaChaRef = useRef<null | HTMLDivElement>(null)
 	const message = [
 		{
 			id: '01',
-			message: 'hello there',
+			message: 'Hello there',
 			self: false,
 			timeStamp: '13:15',
-            user: chatId.name,
-            url: chatId.url
+			user: chatId.name,
+			url: chatId.url,
 		},
 		{
 			id: '02',
-			message: 'How its going',
+			message: "How's it going",
 			self: true,
 			timeStamp: '13:15',
-            user: chatId.name,
-            url: chatId.url
+			user: chatId.name,
+			url: chatId.url,
 		},
 	]
+	useEffect(() => {
+		messaChaRef.current?.scrollIntoView({ behavior: 'smooth', block: 'end' })
+	}, [])
+
 	return (
 		<div className="w-full overflow-x-hidden overflow-y-scroll h-[72vh] [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
 			{message?.map((value, index) => (
 				<div className="max-h-10 mb-10">
-                    
 					<MessageElement messageInfo={value} />
+					<div ref={messaChaRef} />
 				</div>
 			))}
 		</div>
