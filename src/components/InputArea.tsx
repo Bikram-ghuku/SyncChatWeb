6515a0557ef3fb46e0314f47576e9afc8ff648f3
Bikram Ghuku <bikramghuku05@gmail.com>
2024-03-12 19:19:28 +0530

    chore: added message sending to socketio

--- src/components/InputArea.tsx
+++ src/components/InputArea.tsx
@@ -1,15 +1,29 @@
 'use client'
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { Smile, Mic, Paperclip, SendHorizonal } from 'lucide-react'
-function InputArea() {
+import { socketContext } from '@/provider/socketProvider'
+
+function InputArea({chatId}:{chatId: string}) {
+	const [text, setText] = useState<null | string>(null);
+	const socket = useContext(socketContext);
+
+	socket.on('message', (data)=>{
+		console.log(data)
+	})
+
+	const sendMsg = () => {
+		console.log("Sending data...")
+		const data = {jwt: localStorage.getItem("jwt"), msg: text, chatId: chatId}
+		socket.emit('message', data)
+	}
 	return (
 		<div className="flex h-full items-center md:pl-10 w-full">
 			<Button variant="ghost" size="icon" className="mr-2">
 				<Smile />
 			</Button>
-			<Input type="text" placeholder="Type Message..." className="" />
+			<Input type="text" placeholder="Type Message..." className="" onChange={(e) => setText(e.target.value)}/>
 			<Button variant="ghost" size="icon" className="md:ml-5 md:mr-2 ml-1">
 				<Mic />
 			</Button>
@@ -19,6 +33,7 @@ function InputArea() {
 			<Button
 				size="icon"
 				className="mr-1 md:mr-5 ml-1 md:ml-5 md:w-[10%] bg-[#00a3ff] gap-2 p-5 w-[15%]"
+				onClick={() => sendMsg()}
 			>
 				Send <SendHorizonal size="1rem" />
 			</Button>
