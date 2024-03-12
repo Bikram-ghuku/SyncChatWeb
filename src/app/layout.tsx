327cd28f1547c70c4b6b73504314d0b27cbaa4e8
Bikram Ghuku <bikramghuku05@gmail.com>
2024-03-12 19:08:16 +0530

    chore: added socketProvider to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
+import SocketProvider from '@/provider/socketProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.className}>
+				<SocketProvider>
 				<ThemeProvider
 					attribute="class"
 					defaultTheme="system"
@@ -26,6 +28,7 @@ export default function RootLayout({
 				>
 					{children}
 				</ThemeProvider>
+				</SocketProvider>
 			</body>
 		</html>
 	)
