ada7091b47e115789b7b72e54a3eb49c135e2b20
Bikram Ghuku <bikramghuku05@gmail.com>
2024-03-17 22:23:26 +0530

    refactor: cleanup unwanted code

--- src/app/(user)/chat/[chatId]/page.tsx
+++ src/app/(user)/chat/[chatId]/page.tsx
@@ -11,25 +11,6 @@ type Props = {
 }
 
 export default function ChatPage({ params: { chatId } }: Props) {
-	console.log(chatId)
-	interface Chat {
-		id: string
-		members: string[]
-	}
-	const [chats, setChats] = useState<Chat[]>([])
-
-	useEffect(() => {
-		fetch(process.env.NEXT_PUBLIC_API_URL + '/channels/channels', {
-			method: 'GET',
-			headers: {
-				authorization: 'Bearer ' + localStorage.getItem('token'),
-			},
-		})
-			.then(res => res.json())
-			.then(data => {
-				setChats(data)
-			})
-	}, [])
 	return (
 		<main className="dark:bg-[#2f3142] flex flex-col h-screen bg-[#f5f5f5]">
 			<Header />
