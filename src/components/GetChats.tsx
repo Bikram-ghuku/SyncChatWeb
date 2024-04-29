93c7ebc0546fb46b424329f6420696727d6599c7
Bikram Ghuku <bikramghuku05@gmail.com>
2024-04-29 09:46:24 +0530

    feat: added endpoints for add chat button

--- src/components/GetChats.tsx
+++ src/components/GetChats.tsx
@@ -1,6 +1,6 @@
 'use client'
-import React from 'react'
-import { MessageSquarePlus } from 'lucide-react'
+import React, { useState } from 'react'
+import { Loader2, MessageSquarePlus } from 'lucide-react'
 import { Button } from './ui/button'
 import {
 	Dialog,
@@ -11,8 +11,18 @@ import {
 	DialogTrigger,
 } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
+import axios from 'axios'
 
 function GetChats() {
+	const [email, setEmail] = useState<string>('');
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const handleCreateChat = () => {
+		axios.post(process.env.NEXT_PUBLIC_API_URL + '/channels/addChannels', {email}, {
+			headers: {
+				'Authorization': `Bearer ${localStorage.getItem('jwt')}`
+			}
+		})
+	}
 	return (
 		<div>
 			<Dialog>
@@ -27,9 +37,12 @@ function GetChats() {
 						
 						<DialogDescription>
 							<br/>
-							<Input />
+							<Input onChange={(e) => setEmail(e.target.value)} type="email" autoCapitalize="none" autoComplete="email" autoCorrect="off"/>
 							<br/>
-							<Button variant={'default'}>Add User</Button>
+							<Button disabled={isLoading} variant={'default'} onClick={() => handleCreateChat()}>
+								{isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+								Add User
+							</Button>
 						</DialogDescription>
 					</DialogHeader>
 				</DialogContent>
