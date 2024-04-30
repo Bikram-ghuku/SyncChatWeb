e6a14552696b7894878f0ce6161388cfb8480886
Bikram Ghuku <bikramghuku05@gmail.com>
2024-04-30 19:11:17 +0530

    feat: added enter button send

--- src/components/InputArea.tsx
+++ src/components/InputArea.tsx
@@ -9,6 +9,7 @@ function InputArea({ chatId }: { chatId: string }) {
 	const [text, setText] = useState<null | string>(null)
 	const socket = useContext(socketContext)
 	const textAreaRef = useRef<null | HTMLInputElement>(null)
+	const submitRef = useRef<null | HTMLButtonElement>(null);
 
 	const sendMsg = () => {
 		if (!text) return
@@ -24,6 +25,12 @@ function InputArea({ chatId }: { chatId: string }) {
 		textAreaRef.current ? (textAreaRef.current.value = '') : null
 		setText('')
 	}
+
+	const checkEnter = (e: any) => {
+		if(e.keyCode === 13){
+			submitRef.current?.click()
+		}
+	}
 	return (
 		<div className="flex h-full items-center md:pl-10 w-full">
 			<Button variant="ghost" size="icon" className="mr-2">
@@ -35,6 +42,7 @@ function InputArea({ chatId }: { chatId: string }) {
 				className=""
 				onChange={e => setText(e.target.value)}
 				ref={textAreaRef}
+				onKeyDown={(e) => checkEnter(e)}
 			/>
 			<Button variant="ghost" size="icon" className="md:ml-5 md:mr-2 ml-1">
 				<Mic />
@@ -46,6 +54,7 @@ function InputArea({ chatId }: { chatId: string }) {
 				size="icon"
 				className="mr-1 md:mr-5 ml-1 md:ml-5 md:w-[10%] bg-[#00a3ff] gap-2 p-5 w-[15%]"
 				onClick={() => sendMsg()}
+				ref={submitRef}
 			>
 				Send <SendHorizonal size="1rem" />
 			</Button>
