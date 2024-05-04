a8f13e2a97a11a8a0c1d925bfcdb3d555dca79c9
Bikram Ghuku <bikramghuku05@gmail.com>
2024-05-04 12:30:45 +0530

    ui: show loading when loading for github sign-in

--- src/components/user-auth-form.tsx
+++ src/components/user-auth-form.tsx
@@ -16,6 +16,7 @@ interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {
 
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
 	const [isLoading, setIsLoading] = useState<boolean>(false)
+	const [isGitLoad, setIsGitLoad] = useState<boolean>(false)
 	const [email, setEmail] = useState<string>('')
 	const [pswd, setPswd] = useState<string>('')
 	const [name, setName] = useState<string>('')
@@ -25,6 +26,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
 	const data = searchParams.get('code');
 	useEffect(() => {
 		if(!(data == null || data == '')){
+			setIsGitLoad(true)
 			axios.post(process.env.NEXT_PUBLIC_API_URL + '/users/ghreg', {
 				code: data
 			}).then((data) => {
@@ -133,7 +135,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
 							? 'Register with Email'
 							: 'Login with email'}
 					</button>
-					<Button onClick={(e) => router.push(`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GH_CLIENT_ID}`)}>Continue with Github</Button>
+					<Button onClick={(e) => router.push(`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GH_CLIENT_ID}`)} disabled={isGitLoad}>{isGitLoad && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}Continue with Github</Button>
 					<Toaster />
 				</div>
 			</form>
